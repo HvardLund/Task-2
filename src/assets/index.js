ed0b88de370bedf5e36a8ce99379c107aae1b5bc
Håvard Lund <havard.lund@no.experis.com>
2023-01-20 15:39:22 +0100

    feat: implemented translation functionality

---
+++ src/assets/index.js
@@ -0,0 +1,58 @@
+import a from "./signs/a.png"
+import b from "./signs/b.png"
+import c from "./signs/c.png"
+import d from "./signs/d.png"
+import e from "./signs/e.png"
+import f from "./signs/f.png"
+import g from "./signs/g.png"
+import h from "./signs/h.png"
+import i from "./signs/i.png"
+import j from "./signs/j.png"
+import k from "./signs/k.png"
+import l from "./signs/l.png"
+import m from "./signs/m.png"
+import n from "./signs/n.png"
+import o from "./signs/o.png"
+import p from "./signs/p.png"
+import q from "./signs/q.png"
+import r from "./signs/r.png"
+import s from "./signs/s.png"
+import t from "./signs/t.png"
+import u from "./signs/u.png"
+import v from "./signs/v.png"
+import w from "./signs/w.png"
+import x from "./signs/x.png"
+import y from "./signs/y.png"
+import z from "./signs/z.png"
+
+const images = {
+    "a":a,
+    "b":b,
+    "c":c,
+    "d":d,
+    "e":e,
+    "f":f,
+    "g":g,
+    "h":h,
+    "i":i,
+    "j":j,
+    "k":k,
+    "l":l,
+    "m":m,
+    "n":n,
+    "o":o,
+    "p":p,
+    "q":q,
+    "r":r,
+    "s":s,
+    "t":t,
+    "u":u,
+    "v":v,
+    "w":w,
+    "x":x,
+    "y":y,
+    "z":z
+}
+
+
+export default images
