eae5837c893621665c52a66d162112abb5817e70
HvardLund <122644940+HvardLund@users.noreply.github.com>
2023-01-26 10:00:20 +0100

    Revert "Revert "Working on username storage and treatment throughout app""

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+const apiKey = process.env.REACT_APP_API_KEY
+export const createHeaders = () => {
+    return {
+        'Content-Type': 'application/json',
+        'x-api-key': apiKey
+    }
+}
+
+//export const handleRespons = () => {
+//
+//}
--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,21 +1,45 @@
-import { useForm } from "react-hook-form";
+import { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { loginUser } from "../../api/user";
+import { storageSave } from '../../utils/storage';
+import { useHistory } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
+
 const usernameConfig = {
     required: true,
     minLength: 3
 }
 
 const LoginForm = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm();
+    //Hooks
+    const { register, handleSubmit, formState: { errors }} = useForm();
+    const { user, setUser } = useUser()
+    //Local state
+    const [ loading, setLoading ] = useState(false)
+    const [apiError, setApiError ] = useState(null)
 
-    const onSubmit = (data) => {
-        console.log(data)
-    }
+    //Side effects
+    useEffect(() => {
+        console.log("User has changed", user)
+
+    }, [ user ]) //Empty Deps - Only run once
+
+    //Event handlers
+    const onSubmit = async ({ username }) => {
+        setLoading(true);
+        const [ error, userResponse ] = await loginUser(username)
+        if (error!==null){
+            setApiError(error)
+        }
+        if (userResponse !== null){
+            storageSave('translation-user', userResponse)
+            setUser(userResponse)
+        }
+        setLoading(false);
+    };
+
     console.log(errors);
-
+    //Render functions
     const errorMessage = (() => {
         if (!errors.username){
             return null
@@ -44,7 +68,9 @@ const LoginForm = () => {
                     { errorMessage }
                 </fieldset>
 
-                <button type="submit">Continue</button>
+                <button type="submit" disabled={ loading}>Continue</button>
+            { loading && <p>Logging in...</p>}
+            { apiError && <p> { apiError }</p> }
             </form>
         </>
 
