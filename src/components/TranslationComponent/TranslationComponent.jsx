4f609f01363fc38c5fec1ba810771004e7c8f0cd
Your Name <sverrevinje@gmail.com>
2023-01-26 17:14:31 +0100

    removed unused code

--- src/components/TranslationComponent/TranslationComponent.jsx
+++ src/components/TranslationComponent/TranslationComponent.jsx
@@ -14,9 +14,6 @@ function TranslationComponent() {
     const translations = useSelector((state) => state.updateUser.translations)
     const alphabet = "abcdefghijklmnopqrstuvwxyz ".split("")
     const dispatch = useDispatch();
-    const [ loading, setLoading ] = useState(false)
-    const [apiError, setApiError ] = useState(null)
-    const [translationList, setTranslationList] = useState(translations)
 
     const handleArrowClick = () => {
         dispatch(updateTranslations(inputContent));
@@ -41,8 +38,6 @@ function TranslationComponent() {
                 <input maxLength={40} placeholder= {"Type in text..."} className={styles.inputField} type="text" onChange={updateInputContent}></input>
                 <div onClick={handleArrowClick} className={styles.translateButton}>{'>'}</div>
             </div>
-            { loading && <p>Saving, this might take take a few seconds...</p>}
-            { apiError && <p> { apiError }</p> }
             <div className={styles.translationContainer}>
                 {displayedText.map(element => {
                     return alphabet.includes(element) && <img key={generateKey()} className={styles.sign} alt={"Sign"} src = {images[element]}></img>})}
